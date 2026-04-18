e37fe924e2696d1ab553e410d06c3ae7e0ed6ef1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-18 18:00:38 -0400

    `deno` is still needed during build

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,6 +12,7 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 echo "Building package and its dependencies..."
 echo "---------------------------------------------------------------"
+make-aur-package deno-stable-bin
 make-aur-package bun-bin
 make-aur-package python-emoji-country-flag
 make-aur-package aria2p
@@ -19,3 +20,4 @@ make-aur-package varia
 
 # yt-dlp gives a warning that only deno is supported by default
 sed -i -e "s|default=\['deno'\]|default=['bun']|" /usr/lib/python*/site-packages/yt_dlp/options.py
+pacman -Rdd --noconfirm deno-stable-bin
